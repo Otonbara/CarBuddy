4e313ba200689fb65c8afefd2fbe046bf68103af
Otonbara <140208396+Otonbara@users.noreply.github.com>
2025-04-27 18:21:38 +0100

    Refactor code structure for improved readability and maintainability

--- src/pages/Hero.tsx
+++ src/pages/Hero.tsx
@@ -1,74 +1,217 @@
+import { useState, useRef, useEffect } from "react";
 import { motion } from "framer-motion";
-import HeroCity from "../assets/Hero_City.png"
-import MovingCar from "../assets/Car.png"
-import MovingCloud from "../assets/Cloud.png"
+import HeroCity from "../assets/Hero_City.png";
+import MovingCar from "../assets/Car.png";
+import MovingCloud from "../assets/Cloud.png";
+import Sedan from "../assets/sedan_car.png";
+import SUV from "../assets/suv_car.png";
+import Truck from "../assets/truck_car.png";
+import Van from "../assets/van_car.png";
+import Coupe from "../assets/coupe_car.png";
 
-export default function Hero () {
-    return (
-        <main className="bg-[#87CEEB]/60 lg:px-[100px] lg:py-[30px] px-[30px] py-[30px]">
-            <div className="flex flex-col-reverse lg:flex-row items-center justify-center gap-y-10 lg:gap-2.5">
-                {/* Hero Text */}
-                <div>
-                    <motion.div
-                        initial={{ opacity: 0, y: 50 }}
-                        whileInView={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 0.8 }}
-                        viewport={{ once: true }}>
-                        <h1 className="text-center lg:text-left text-6xl font-extrabold font-[Poppins]">
-                            Hop In, Buddy — Your Next Adventure Awaits!
-                        </h1>
-                    </motion.div>
-                </div>
-                {/* Hero Image */}
-                {/* Road & Skyscraper */}
-                <motion.div
-                    initial={{ opacity: 0, y: 50 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.8 }}
-                    viewport={{ once: true }}
-                    className="w-[80vw] relative mx-auto overflow-hidden">
-                    <img 
-                        src={HeroCity} 
-                        alt="Hero City"
-                        className="w-full h-full object-cover"/>
-                    {/* Moving Car */}
-                    <motion.div
-                        initial={{opacity:0, x: -100 }}
-                        whileInView={{opacity:1, x: 0 }}
-                        transition={{ duration: 0.8, delay: 1 }}
-                        viewport={{ once: true }}
-                        className="absolute bottom-[-25px] left-[-10px] animate-car">
-                        <img 
-                            src={MovingCar} 
-                            alt="Moving Car"
-                            className="w-[150px] object-cover"/>
-                    </motion.div>
-                    {/* Moving Cloud */}
-                    <motion.div
-                        initial={{opacity:0, x: 100 }}
-                        whileInView={{opacity:1, x: 0 }}
-                        transition={{ duration: 0.8, delay: 1 }}
-                        viewport={{ once: true }}
-                        className="absolute top-[-25px] right-[-10px] animate-cloud">
-                        <img 
-                            src={MovingCloud} 
-                            alt="Moving Cloud"
-                            className="w-[150px] object-cover"/>
-                    </motion.div>
-                </motion.div>
-            </div>
-            <div className="font-[DM_Sans] flex justify-center mt-8 gap-4">
-                <button 
-                    className="bg-[#696969] font-bold text-sm px-4 py-2 rounded-full hover:bg-[#696969]/70 
-                    transition duration-300 ease-in-out">
-                    <a href="">Book Now</a>
-                </button>
-                <button 
-                    className="bg-[#696969] font-bold text-sm px-4 py-2 rounded-full hover:bg-[#696969]/70 
-                    transition duration-300 ease-in-out">
-                    <a href="">Browse Cars</a>
-                </button>
-            </div>
-        </main>
-    )
+export default function Hero() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedVehicle, setSelectedVehicle] = useState("Select Vehicle Type");
+
+  const vehicles = [
+    { label: "Sedan", img: Sedan },
+    { label: "SUV", img: SUV },
+    { label: "Truck", img: Truck },
+    { label: "Van", img: Van },
+    { label: "Coupe", img: Coupe },
+  ];
+
+  const dropdownRef = useRef(null);
+
+  // Close dropdown when clicked outside
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (dropdownRef.current && !(dropdownRef.current as HTMLElement).contains(event.target as Node)) {
+      setIsOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, []);
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  interface Vehicle {
+    label: string;
+    img: string;
+  }
+
+  const handleSelect = (vehicle: Vehicle): void => {
+    setSelectedVehicle(vehicle.label);
+    setIsOpen(false);
+  };
+
+  return (
+    <main>
+      <div className="relative bg-[#87CEEB]/60 lg:px-[100px] lg:py-[60px] px-[30px] py-[60px] z-20">
+        <div className="flex flex-col-reverse lg:flex-row items-center justify-center gap-y-10 lg:gap-2.5">
+          {/* Hero Text */}
+          <div>
+            <motion.div
+              initial={{ opacity: 0, y: 50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8 }}
+              viewport={{ once: true }}>
+              <h1 className="text-center lg:text-left text-6xl font-extrabold font-[Poppins]">
+                Hop In, Buddy — Your Next Adventure Awaits!
+              </h1>
+            </motion.div>
+          </div>
+          {/* Hero Image */}
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.8 }}
+            viewport={{ once: true }}
+            className="w-[80vw] relative mx-auto overflow-hidden">
+            {/* Road & Skyscraper */}
+            <motion.div
+              initial={{ opacity: 0, y: -50 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8, delay: 0.5 }}
+              viewport={{ once: true }}>
+              <img
+                src={HeroCity}
+                alt="Hero City"
+                className="w-full h-full object-cover"/>
+            </motion.div>
+            {/* Moving Car */}
+            <motion.div
+              initial={{ opacity: 0, x: -100 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.8, delay: 1 }}
+              viewport={{ once: true }}
+              className="absolute bottom-[-25px] left-[-10px] animate-car">
+              <img
+                src={MovingCar}
+                alt="Moving Car"
+                className="w-[150px] object-cover"/>
+            </motion.div>
+            {/* Moving Cloud */}
+            <motion.div
+              initial={{ opacity: 0, x: 100 }}
+              whileInView={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.8, delay: 1 }}
+              viewport={{ once: true }}
+              className="absolute top-[-25px] right-[-10px] animate-cloud">
+              <img
+                src={MovingCloud}
+                alt="Moving Cloud"
+                className="w-[150px] object-cover"/>
+            </motion.div>
+          </motion.div>
+        </div>
+        <div className="font-[DM_Sans] flex justify-center mt-8 gap-4">
+          <button className="bg-[#696969] font-bold text-sm px-4 py-2 rounded-full hover:bg-[#696969]/70 transition duration-300 ease-in-out hover:shadow-sm shadow-black">
+            <a href="">Book Now</a>
+          </button>
+          <button className="bg-[#696969] font-bold text-sm px-4 py-2 rounded-full hover:bg-[#696969]/70 transition duration-300 ease-in-out hover:shadow-sm shadow-black">
+            <a href="">Browse Cars</a>
+          </button>
+        </div>
+      </div>
+
+      {/* Transparent Floating Platform */}
+      <div className="absolute flex flex-col lg:top-[83%] md:top-[85%] top-[130%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 
+                bg-white/50 backdrop-blur-md p-6 rounded-lg shadow-lg z-30 font-[Poppins]
+                w-full lg:w-[90%] md:w-[90%]">
+        <h2 className="text-2xl font-bold text-center mb-6">Find Your Ride</h2>
+
+        <form className="flex flex-col lg:flex-row md:items-center md:justify-between gap-4">
+
+          {/* Type of Vehicle */}
+          <div
+            ref={dropdownRef}
+            className="flex flex-col items-center w-full md:w-auto relative">
+            <label className="text-sm font-semibold mb-1">
+              Vehicle Type
+            </label>
+            <button
+              type="button"
+              onClick={toggleDropdown}
+              className="p-3 w-full md:w-48 border border-gray-300 rounded-md bg-transparent focus:outline-none flex justify-between items-center">
+              {selectedVehicle}
+              <svg
+                className={`w-5 h-5 ml-2 transition-transform ${isOpen ? "rotate-180" : ""}`}
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24">
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 9l-7 7-7-7"/>
+              </svg>
+            </button>
+
+            {isOpen && (
+              <div className="absolute mt-2 w-full md:w-48 bg-white border border-gray-300 rounded-md shadow-md z-50">
+                {vehicles.map((vehicle, index) => (
+                  <div
+                    key={index}
+                    onClick={() => handleSelect(vehicle)}
+                    className="flex items-center p-2 hover:bg-gray-100 cursor-pointer">
+                    <img src={vehicle.img} alt={vehicle.label} className="w-[50px] h-[50px] mr-3"/>
+                    <span>{vehicle.label}</span>
+                  </div>
+                ))}
+              </div>
+            )}
+          </div>
+
+          {/* Location */}
+          <div className="flex flex-col items-center w-full md:w-auto">
+            <label className="text-sm font-semibold mb-1">Select Location</label>
+            <select className="p-3 w-full md:w-48 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#696969]">
+              <option value="lagos">Lagos State</option>
+              <option value="abuja">Abuja State</option>
+              <option value="rivers">Rivers State</option>
+              <option value="bayelsa">Bayelsa State</option>
+            </select>
+          </div>
+
+          {/* Pickup Date and Time */}
+          <div className="flex flex-col items-center w-full md:w-auto">
+            <label className="text-sm font-semibold mb-1">Pickup Date & Time</label>
+            <input
+              type="datetime-local"
+              className="p-3 w-full md:w-56 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#696969]"/>
+          </div>
+
+          {/* Return Date and Time */}
+          <div className="flex flex-col items-center w-full md:w-auto">
+            <label className="text-sm font-semibold mb-1">Return Date & Time</label>
+            <input
+              type="datetime-local"
+              className="p-3 w-full md:w-56 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-[#696969]"
+            />
+          </div>
+
+          {/* Search Button */}
+          <button
+            type="submit"
+            className="bg-[#696969] font-bold text-sm p-3 w-full md:w-auto hover:bg-[#696969]/70 
+                        transition duration-300 ease-in-out text-white rounded-md">
+            Search
+          </button>
+        </form>
+      </div>
+
+      {/* Gradient Background */}
+      <div className="w-[600px] h-[400px] bg-[#87CEEB] rounded-full 
+                absolute lg:top-[50%] left-[50%] translate-x-[-50%] translate-y-[15%] blur-[150px] z-10"></div>
+      <div className="relative flex flex-col items-center justify-center lg:px-[100px] lg:py-[200px] px-[30px] pt-[560px] z-20">
+        {/* AVAILABLE CARS CAROUSEL */}
+        <h2 className="text-center text-4xl font-bold font-[Poppins]">Available Cars</h2>
+      </div>
+    </main>
+  );
 }

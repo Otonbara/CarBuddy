b4e07a08c88a4b51349a941afce1429666a44510
Otonbara <140208396+Otonbara@users.noreply.github.com>
2025-04-30 15:04:35 +0100

    refactor: remove unused Swiper imports from Avail_Car_Carousel component

--- src/components/Avail_Car_Carousel.tsx
+++ src/components/Avail_Car_Carousel.tsx
@@ -1,8 +1,2 @@
-import { Swiper, SwiperSlide } from "swiper/react";
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import { Navigation, Pagination, Autoplay } from 'swiper/modules'
-
 export default function Avail_Car_Carousel() {
 }
